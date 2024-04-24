4cf4bc96b482ff2ecf903a03d5851b2cc87ca877
Kravmi <144707588+Kravmi@users.noreply.github.com>
2024-04-24 16:54:24 +0300

    Создали управление персонажем и работа с картой..

--- hero.py
+++ hero.py
@@ -31,6 +31,20 @@ class Hero():
         base.accept('d' + '-repeat', self.turn_right)
         base.accept('a', self.turn_left)
         base.accept('a' + '-repeat', self.turn_left)
+        base.accept('arrow_left', self.left)
+        base.accept('arrow_left' + '-repeat', self.left)
+        base.accept('arrow_right', self.right)
+        base.accept('arrow_right' + '-repeat', self.right)
+        base.accept('arrow_up', self.forward)
+        base.accept('arrow_up' + '-repeat', self.forward)
+        base.accept('arrow_down', self.back)
+        base.accept('arrow_down' + '-repeat', self.back)
+        base.accept('space', self.up)
+        base.accept('space' + '-repeat', self.up)
+        base.accept('control', self.down)
+        base.accept('control' + '-repeat', self.down)
+        base.accept('g', self.changeMode)
+        base.accept('g' + '-repeat', self.changeMode)
 
     def changeView(self):
         if self.camera_on == True:
@@ -46,25 +60,81 @@ class Hero():
 
     # определяет движение игрока в режиме наблюдателя
     def just_move(self, angle):
-        pass
+        pos = self.look_at(angle)
+        self.hero.setPos(pos)
 
     # Определяет движение игрока в основном режиме
     def try_move(self, angle):
-        pass
+        pos = self.look_at(angle)
+        if self.land.isEmpty(pos):
+            pos = self.land.findHighestEmpty(pos)
+            self.hero.setPos(pos)
+        else:
+            pos = pos[0], pos[1], pos[2] + 1
+            if self.land.isEmpty(pos):
+               self.hero.setPos(pos) 
 
     # определяет вид движения 
     # в зависимости от свойства self.mode
     # если равно true то вызываем just_move
     # иначе try_move
     def move_to(self, angle):
-        pass
+        if self.mode == True:
+            self.just_move(angle)
+        else:
+            self.try_move(angle)
 
     def look_at(self, angle):
         from_x = round(self.hero.getX())
         from_z = round(self.hero.getZ())
         from_y = round(self.hero.getY())
-        dx, dy = self.hero.check_dir(self, angle)
+        dx, dy = self.check_dir(angle)
         return from_x+dx, from_y+dy, from_z
 
     def check_dir(self, angle):
-        pass
+        if angle >= 0 and angle <= 20:
+            return 0, -1
+        elif angle > 20 and angle <= 65:
+            return 1, -1
+        elif angle > 65 and angle <= 110:
+            return 1, 0
+        elif angle > 110 and angle <= 155:
+            return 1, 1
+        elif angle > 155 and angle <= 200:
+            return 0, 1
+        elif angle > 200 and angle <= 245:
+            return -1, 1
+        elif angle > 245 and angle <= 290:
+            return -1, 0
+        elif angle > 290 and angle <= 335:
+            return -1, -1
+        else: 
+            return 0, -1
+
+    def back(self):
+        angle = (self.hero.getH()+180) % 360
+        self.move_to(angle)
+
+    def forward(self):
+        angle = (self.hero.getH()+0) % 360
+        self.move_to(angle)
+        
+    def left(self):
+        angle = (self.hero.getH() + 90) % 360
+        self.move_to(angle)
+
+    def right(self):
+        angle = (self.hero.getH()+270) % 360
+        self.move_to(angle)
+
+    def up(self):
+        self.hero.setZ(self.hero.getZ() + 1)
+
+    def down(self):
+        self.hero.setZ(self.hero.getZ() - 1)
+
+    def changeMode(self):
+        if self.mode == True:
+            self.mode = False
+        else:
+            self.mode = True
--- mapmanager.py
+++ mapmanager.py
@@ -16,6 +16,7 @@ class Mapmanager():
         self.color = self.get_color(position[2])
         self.block.setColor(self.color)
         self.block.reparentTo(self.land)
+        self.block.setTag('at', str(position))
 
     def start_new(self):
         self.land = render.attachNewNode('Land')
@@ -44,3 +45,19 @@ class Mapmanager():
         else: 
             return self.colors[-1]
             
+    def isEmpty(self, pos):
+        blocks = self.findBlocks(pos)
+        if blocks:
+            return False
+        else:
+            return True
+
+    def findBlocks(self, pos):
+        return self.land.findAllMatches('=at=' + str(pos))
+ 
+    def findHighestEmpty(self, pos):
+        x, y, z = pos
+        z = 1
+        while not self.isEmpty((x, y, z)):
+            z += 1
+        return(x, y, z)
